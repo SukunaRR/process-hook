31ea13b30d7d17f51e1232c60cf9c519d8e28b0c
SukunaRR <166194825+SukunaRR@users.noreply.github.com>
2025-05-31 18:04:49 +0530

    Update PackageManagerSpoofHook.java

--- app/src/main/java/com/rifsxd/processhook/PackageManagerSpoofHook.java
+++ app/src/main/java/com/rifsxd/processhook/PackageManagerSpoofHook.java
@@ -1,53 +1,26 @@
-package com.rifsxd.spoofhook;
+package com.rifsxd.processhook;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.ApplicationInfo;
-
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class PackageManagerSpoofHook {
+public class PackageNameSpoofHook {
 
-    private static final String ORIGINAL_PKG = "com.pubg.imobile";
-    private static final String SPOOFED_PKG = "com.tencent.ig";
-
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
-        if (!"com.csdroid.pkg".equals(lpparam.packageName)) return;
-
-        Class<?> pmClass = XposedHelpers.findClassIfExists("android.app.ApplicationPackageManager", lpparam.classLoader);
-        if (pmClass == null) return;
-
-        XposedHelpers.findAndHookMethod(pmClass, "getApplicationInfo",
-                String.class, int.class, new XC_MethodHook() {
+    public static void hook(XC_LoadPackage.LoadPackageParam lpparam) {
+        try {
+            XposedHelpers.findAndHookMethod(
+                "android.app.Application",              // Class
+                lpparam.classLoader,
+                "getPackageName",                       // Method
+                new XC_MethodReplacement() {
                     @Override
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        if (ORIGINAL_PKG.equals(param.args[0])) {
-                            param.args[0] = SPOOFED_PKG;
-                        }
+                    protected Object replaceHookedMethod(MethodHookParam param) {
+                        return "com.tencent.ig";        // Spoofed package name
                     }
-                });
-
-        XposedHelpers.findAndHookMethod(pmClass, "getPackageInfo",
-                String.class, int.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        if (ORIGINAL_PKG.equals(param.args[0])) {
-                            param.args[0] = SPOOFED_PKG;
-                        }
-                    }
-                });
-
-        XposedHelpers.findAndHookMethod(pmClass, "getInstallerPackageName",
-                String.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        if (ORIGINAL_PKG.equals(param.args[0])) {
-                            param.args[0] = SPOOFED_PKG;
-                        }
-                    }
-                });
+                }
+            );
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 }
